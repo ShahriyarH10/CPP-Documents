cea60cde14e8d33984b7c3e6bc4a75b1a6c83a3e
ShahriyarH10 <md.shahriyarhasan2002me@gmail.com>
2023-11-13 00:08:25 +0600

    update Lecture 6

---
+++ Class Exr/Lecture 6_String/string01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#define PRAISE " What a super marvelous name!"
+using namespace std;
+
+int main() {
+    char name[40];
+    cout << "What is your First Name?" << endl;
+    cin >> name;
+    cout << "Hello " << name <<  PRAISE << endl;
+    cout << "Your name of " << strlen(name) << " letters occupies" << sizeof name << " memory" << endl;
+    return 0;
+}
