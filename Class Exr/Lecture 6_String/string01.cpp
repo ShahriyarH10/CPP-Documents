b2966aa3386414e3d4f27f7a843aa8d5712b46bd
Shahriyar Hasan <85627941+ShahriyarH10@users.noreply.github.com>
2023-11-13 00:20:49 +0600

    Update string01.cpp
    aligning the syntax

--- Class Exr/Lecture 6_String/string01.cpp
+++ Class Exr/Lecture 6_String/string01.cpp
@@ -7,7 +7,7 @@ int main() {
     char name[40];
     cout << "What is your First Name?" << endl;
     cin >> name;
-    cout << "Hello " << name <<  PRAISE << endl;
+    cout << "Hello " << name << PRAISE << endl;
     cout << "Your name of " << strlen(name) << " letters occupies" << sizeof name << " memory" << endl;
     return 0;
 }
